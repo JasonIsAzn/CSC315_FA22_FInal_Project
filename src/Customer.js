9a283f2942c214532ad20cfc9298bfabaa3fe7c6
JasonIsAzn <jasonisazn@tamu.edu>
2022-10-26 16:28:54 -0500

    intial files created

---
+++ src/Customer.js
@@ -0,0 +1,13 @@
+import './Customer.css';
+
+function Customer() {
+    return (
+      <div className="Customer">
+        <header className="Customer-header">
+            <p>Customer</p>
+        </header>
+      </div>
+    );
+}
+
+export default Customer;
---
+++ src/Manager.js
@@ -0,0 +1,13 @@
+import './Manager.css';
+
+function Manager() {
+    return (
+      <div className="Manager">
+        <header className="Manager-header">
+            <p>Manager</p>
+        </header>
+      </div>
+    );
+}
+
+export default Manager;
