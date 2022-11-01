98a7277882309abb8b261ac9c5a43ef130f59082
JasonIsAzn <jasonisazn@tamu.edu>
2022-10-31 22:45:42 -0500

    Testing Connection. Works but server-side get-request is ran seperately

--- src/frontend/pages/Customer.js
+++ src/frontend/pages/Customer.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+
 
 export default function Customer() {
+
+  /*TESTING DATABASE CONNECTION HERE - GET*/
+
+  const getItems = async() => {
+    try {
+      const response = await fetch("http://localhost:5000/items") // get request
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getItems();
+  })
+
+  
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center text-4xl text-gray-400">
       Customer Page
