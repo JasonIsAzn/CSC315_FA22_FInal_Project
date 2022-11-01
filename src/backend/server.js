e0622c3fc7bf57ff924aefa62be44a195ddd1334
JasonIsAzn <jasonisazn@tamu.edu>
2022-10-31 22:44:29 -0500

    created get items

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require("cors")
 const pool = require("./db");
+
 
 app.use(cors());
 app.use(express.json());
-pool.connect(); // FIXME: may want to try-catch this to ensure connection
 
-// pool.query(...) - used to make queries
+// Add process hook to shutdown pool
+process.on('SIGINT', function() {
+  pool.end();
+  console.log('Application successfully shutdown');
+  process.exit(0);
+});
+
+// ROUTES // 
+// get Items
+app.get("/items" , async (req, res) => {
+  try{
+    const orders = await pool.query("SELECT * FROM item");
+    res.json(orders.rows);
+  } catch {err} {
+    console.error(err.message);
+  }
+});
+
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
