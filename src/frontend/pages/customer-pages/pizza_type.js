dd9b56df6bda43baf7d87f9a099b51db0f04895b
JasonIsAzn <jasonisazn@tamu.edu>
2022-11-04 23:43:43 -0500

    raw data file of pizza type

---
+++ src/frontend/pages/customer-pages/pizza_type.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: "Orginial Cheese",
+        type: 1
+    },
+    {
+        name: "Classic Pepproni",
+        type: 2
+    },
+    {
+        name: "1-Topping",
+        type: 3
+    },
+    {
+        name: "2-4 Topping",
+        type: 4
+    },
+    {
+        name: "Featured Items",
+        type: 5
+    },
+]
