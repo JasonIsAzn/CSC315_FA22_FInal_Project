b7a8c543bde3c077efdfa4f912e3340bcc135d62
trisha-narwekar <trishaprtn@gmail.com>
2022-11-18 00:06:32 -0600

    potato

--- src/frontend/pages/manager-pages/Inventory.js
+++ src/frontend/pages/manager-pages/Inventory.js
@@ -7,6 +7,11 @@ import GlobalContext from "../../context/GlobalContext";
 export default function Inventory() {
   const { listItems, allItems } = useContext(GlobalContext);
 
+  const [itemName, setItemName] = useState("");
+  const [count, setCount] = useState("");
+  const [price, setPrice] = useState("");
+  const [type, setType] = useState("");
+
   const navigate = useNavigate();
 
   // sends the user to the Home page
@@ -35,7 +40,23 @@ export default function Inventory() {
     filterType: "dropdown",
     responsive: "scroll",
   };
-
+  async function addItem() {
+    axios.post("/items/add_item", {
+      name: itemName,
+      price: price,
+      count: count,
+      type: type,
+    }).then(() => {
+      alert("SENT")
+    }).then(() => {
+      setCount(0);
+      setItemName("");
+      setPrice(0.00);
+      setType("");
+    }).catch((err) => {
+      alert(err);
+    })
+  }
   return (
     <div className="h-screen overflow-y-hidden">
       <div className="w-screen flex justify-center mt-16">
@@ -67,14 +88,57 @@ export default function Inventory() {
           Server Mode
         </button>
       </div>
-      <div className="items-center justify-center px-[10%] pt-[3%]">
+      <div className="px-[10%] pt-[3%] flex">
+        <div className="grid-span-3">
         <MUIDataTable
           title={"Items"}
           data={listItems}
           columns={columns}
           options={options}
-        />
+        /></div>
+        <div className="flex justify-center items-center">
+          <div className="h-48 w-3/4 ml-8 grid grid-cols-2 grid-rows-2">
+          <input
+            type="text"
+            className="h-8 mx-[13%] mt-10 border border-1 border-black hover:border-gray-500 focus:ring-0 focus:outline-none rounded-lg text-xl"
+            placeholder="Item Name"
+            onChange={(e) => {
+              setItemName(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            className="h-8 mx-[13%] mt-10 border border-1 border-black hover:border-gray-500 focus:ring-0 focus:outline-none rounded-lg text-xl"
+            placeholder="Count"
+            onChange={(e) => {
+              setCount(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            className="h-8 mx-[13%] border border-1 border-black hover:border-gray-500 focus:ring-0 focus:outline-none rounded-lg text-xl"
+            placeholder="Price"
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            className="h-8 mx-[13%] border border-1 border-black hover:border-gray-500 focus:ring-0 focus:outline-none rounded-lg text-xl"
+            placeholder="Type"
+            onChange={(e) => {
+              setType(e.target.value);
+            }}
+          />
+          </div>
+          <button className="w-32 ml-8 border-[#4FC3F7] border-2 rounded-lg flex justify-center items-center text-[#4FC3F7] text-xl font-bold"
+          onClick={addItem}
+          >
+            Add item
+          </button>
+          </div>
       </div>
+      
     </div>
   );
 }
