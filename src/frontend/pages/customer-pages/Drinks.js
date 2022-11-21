2d80fa4b127a2979fb9746f8e97590b9819fe286
JasonIsAzn <jasonisazn@tamu.edu>
2022-11-21 11:07:17 -0600

    update drink laels

--- src/frontend/pages/customer-pages/Drinks.js
+++ src/frontend/pages/customer-pages/Drinks.js
@@ -7,6 +7,22 @@ export default function Drinks() {
   const { drinks } = useContext(GlobalContext);
   const [selectedDrinks, setSelectedDrinks] = useState(drinks);
   const [selectedDrinksCounts, setSelectedDrinksCounts] = useState([]);
+  let drinksTextFormatted = [];
+  for (let i = 0; i < selectedDrinks.length; ++i) {
+    let formatText = selectedDrinks[i].label;
+    formatText = formatText.replace("_", " ");
+    const texts = formatText.split(" ");
+    if (texts[0] === "ftn") {
+      texts[0] = "Fountain";
+    }
+    if (texts[0] === "mtn") {
+      texts[0] = "Mountain";
+    }
+    for (let j = 0; j < texts.length; ++j) {
+      texts[j] = texts[j][0].toUpperCase() + texts[j].substr(1);
+    }
+    drinksTextFormatted.push(texts.join(" "));
+  }
 
   // Render Page - Initial Load Data
   useEffect(() => {
@@ -189,7 +205,7 @@ export default function Drinks() {
 
       <div>
         <h1 class="text-3xl font-bold ml-20 mb-6 mt-10">Choose Drinks</h1>
-        <div className="grid lg:grid-cols-4 mx-20 mt-5">
+        <div className="grid lg:grid-cols-4 mt-5">
           {drinks.map((drink, index) => (
             <div className="mx-auto">
               <input
@@ -202,9 +218,9 @@ export default function Drinks() {
               <label
                 class=""
                 for={drink.value}
-                className="bg-[#4FC3F7] hover:bg-white hover:text-[#4FC3F7] hover:border-[#4FC3F7] hover:border-2 text-white font-bold mx-auto my-1 p-20 rounded-lg text-l flex justify-center items-center"
+                className="bg-[#4FC3F7] hover:bg-white hover:text-[#4FC3F7] hover:border-[#4FC3F7] hover:border-2 text-white font-bold mx-12 my-1 p-20 rounded-lg text-l flex justify-center items-center"
               >
-                {drink.label}
+                {drinksTextFormatted[index]}
               </label>
 
               <div className="h-10 w-auto mb-5 flex items-center justify-center bg-[#FFF]">
